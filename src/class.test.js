9cf9ec3a6235d486a26483574978e96a5a1cc2a9
Nuri1977 <nurilacka@gmail.com>
2022-01-18 13:56:34 +0100

    Add test for delete function

--- src/class.test.js
+++ src/class.test.js
@@ -1,10 +1,26 @@
 import Tasks from './class';
 
+const tasks = new Tasks();
+
 describe('testing add', () => {
-  const tasks = new Tasks();
 
   it('adding the first task', () => {
     tasks.add({ description: 'task 1' });
     expect(tasks.list.length).toBe(1);
   });
+
+  it('adding and chacking task description', () => {
+    tasks.add({ description: 'task 2' });
+    expect(tasks.list[1].description).toBe('task 2');
+  })
+});
+
+describe('testing delete', () => {
+  const deleteIndex = 1;
+
+  it('test if first element is deleted', () => {
+    tasks.remove(deleteIndex);
+    expect(tasks.list.length).toBe(1);
+  })
+
 });
